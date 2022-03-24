398c7dba9c86fa306d289ea982468b43f6b2584d
Chengcheng Ding <cd034@bucknell.edu>
2022-03-24 16:56:42 -0400

    Updates

---
+++ precache-manifest.4175af680fe6559eed66f03daa9dfdcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82345c8bb62ac2b1eb2e586f67c23835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c4488f49a453839c865",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8332b83a48aee657a8e8",
+    "url": "/static/css/main.bc339672.chunk.css"
+  },
+  {
+    "revision": "9c4488f49a453839c865",
+    "url": "/static/js/2.e527d884.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.e527d884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8332b83a48aee657a8e8",
+    "url": "/static/js/main.5aa40edc.chunk.js"
+  },
+  {
+    "revision": "5cd9967c5bfe33feea3f",
+    "url": "/static/js/runtime-main.46c42b6c.js"
+  }
+]);
